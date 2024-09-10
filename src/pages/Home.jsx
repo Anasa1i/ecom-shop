9d805aa162d0db210d0554cdc1fb880b7ea48496
Anas Ali <anasalimgmt@gmail.com>
2024-09-10 08:16:24 +0500

    incomplete
    making a components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React, {useContext}  from 'react';
+
+//import product context
+import {ProductContext} from '../contexts/ProductContext'
+const Home = () => {
+  //get products from product context
+  const { products } = useContext(ProductContext)
+  console.log(products);
+  return <div>Homepage</div>;
+};
+
+export default Home;
